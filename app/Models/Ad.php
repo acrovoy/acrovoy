fe655c8d1b42e16ef8adacce00c4276a3d18bd59
Stan <sm@odisseyglobal.com>
2025-04-18 13:12:06 +0300

    add saved file Admodel

--- app/Models/Ad.php
+++ app/Models/Ad.php
@@ -1,10 +1,26 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Ad extends Model
 {
-    //
+    use HasFactory;
+
+    // Указываем таблицу, если имя таблицы не соответствует соглашению
+    protected $table = 'ads';
+
+    // Указываем, какие поля могут быть массово присвоены
+    protected $fillable = [
+        'image_path',
+        'link',
+        'date_from',
+        'date_to',
+        'advertiser_name',
+        'advertiser_contact',
+    ];
+
+    // Автоматически добавляем временные метки created_at и updated_at
+    public $timestamps = true;
 }
